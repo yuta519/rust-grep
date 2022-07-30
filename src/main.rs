c65c4385a536b7aa75e74e484f7a218244a2e8c3
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-07-30 23:13:59 +0900

    use formula

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@ use chrono::{DateTime, Local};
 fn main() {
     let dt: DateTime<Local> = Local::now();
     let timestamp: i64 = dt.timestamp();
-    let mut world: &str = "";
-
-    if timestamp % 2 == 0 {
-        println!("{}", timestamp);
-        world = "devided!!!!!!!!!!!!!";
+    let world: &str = if timestamp % 2 == 0 {
+        "devided!!!!!!!!!!!!!"
     } else {
-        world = "non devided!!!!!!!!!!!!!";
-    }
+        "non devided!!!!!!!!!!!!!"
+    };
     println!("Hello {}", world);
 }
