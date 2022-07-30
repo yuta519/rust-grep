4616fc2ac03ce1e0d4c562e67b0487e5228d0c6b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-07-30 21:16:31 +0900

    add unixtime

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use chrono::{DateTime, Local};
 fn main() {
     let world: &str = "world!!!!!!!!!!!!!";
     println!("Hello {}", world);
+    let dt: DateTime<Local> = Local::now();
+    let timestamp: i64 = dt.timestamp();
+
+    println!("{}", timestamp);
 }
