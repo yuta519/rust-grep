9f51972131c10285f82674e3764a1701b577208e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-08-21 23:01:34 +0900

    comment out in unnecesarry line

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use chrono::{DateTime, Local};
 use std::fs::read_to_string;
 
 fn main() {
-    let dt: DateTime<Local> = Local::now();
-    let timestamp: i64 = dt.timestamp();
-    let world: &str = if timestamp % 2 == 0 {
-        "devided!!!!!!!!!!!!!"
-    } else {
-        "non devided!!!!!!!!!!!!!"
-    };
-    println!("Hello {}", world);
+    // let dt: DateTime<Local> = Local::now();
+    // let timestamp: i64 = dt.timestamp();
+    // let world: &str = if timestamp % 2 == 0 {
+    //     "devided!!!!!!!!!!!!!"
+    // } else {
+    //     "non devided!!!!!!!!!!!!!"
+    // };
+    // println!("Hello {}", world);
 
     println!("標準入力テストです。任意の値を入力してください。");
     match std::env::args().nth(1) {
@@ -21,7 +21,6 @@ fn main() {
 
 fn cat_cmd(path: String) {
     println!("\n{}", path);
-    println!("###########################################\n");
     match read_to_string(path) {
         Ok(content) => print!("{}", content),
         Err(reason) => print!("{}", reason),
